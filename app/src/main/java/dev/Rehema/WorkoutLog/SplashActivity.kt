5c5d3ad78689db3eae3daac0f0c7af2756e6d560
rehemae <rehemaephraimnanda@gmail.com>
2022-05-24 20:09:20 +0300

    SplashActivity.kt

---
+++ app/src/main/java/dev/Rehema/WorkoutLog/SplashActivity.kt
@@ -0,0 +1,14 @@
+package dev.Rehema.WorkoutLog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+val intent=Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
